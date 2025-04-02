6558eb909873a176bc5a55383364249c59926b5b
jojofaggoo <156486878+jojofaggoo@users.noreply.github.com>
2025-04-02 16:00:44 -0500

    Create index.php

---
+++ .github/workflows/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>My PHP Website</title>
+</head>
+<body>
+<h1>Welcome to the Cloud!</h1>
+<form action = "" method ="post">
+Your name:
+<br>
+<input type="text" name = "name" size="30" maxlength = "30">
+<br>
+<input type="submit" name = "submit" value="Submit">
+<input type="submit" name = "view" value = "View All">
+</form>
+<?php
+//replace the user and password with your credentials
+$host = "jfcloudlinuxbook-bzh8gsfsftaucrhw.centralus-01.azurewebsites.net";
+$user = "ljfague1";
+$password = "Unity916";
+$db = "visitorDB";
+// connect to the database
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $user, $password, $db,3306);
+if (isset($_POST['submit']))
+{
+$yourName=$_POST['name'];
+//sql statement
+$query = INSERT INTO visitor (visitorName) VALUES ('$yourName');
+if(mysqli_query($conn, $query))
+echo "<p>Hi, $yourName, welcome to my cloud.</p>";
+else
+echo "<p>Hi, $yourName, please try again. </p>";
+}
+//if the View all button is clicked
+if (isset($_POST['view']))
+{
+	$query = "SELECT * FROM visitor";
+	$result = mysqli_query($conn,$query);
+	if(mysqli_num_rows($result)>0)
+	{
+		$display = "<h2>All Visitors </h2>";
+		while($row=mysqli_fetch_assoc($result)){
+			$display .="Name: ".$row["visitorName"]."<br>";
+			$display .="Date Time: " .$row["visitTime"]."<br>";
+			}
+			}
+			echo $display;
+			}
+			//close connection 
+mysqli_close($conn);
+
+?>
+</body>
+</html>
+
+
