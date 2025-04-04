6eeb9eae42d8a5126ec3c335a864ac6439f4d6fd
jojofaggoo <156486878+jojofaggoo@users.noreply.github.com>
2025-04-04 16:39:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
+<meta charset = "utf-8">
 <title>My PHP Website</title>
 </head>
 <body>
@@ -16,19 +16,20 @@ Your name:
 </form>
 <?php
 //replace the user and password with your credentials
-$host = "jfcloudbook.mysql.database.azure.com/wwwroot";
+$host = "jfcloudbook.mysql.database.azure.com";
 $user = "ljfague1";
 $password = "Unity916";
 $db = "visitordb";
 // connect to the database
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $user, $password, $db,1433);
+mysqli_real_connect($conn, $host, $user, $password, $db);
 if (isset($_POST['submit']))
 {
 $yourName=$_POST['name'];
 //sql statement
 $query = "INSERT INTO visitor (visitorName) VALUES ('$yourName')";
 if(mysqli_query($conn, $query))
+{
 echo "<p>Hi, $yourName, welcome to my cloud.</p>";
 else
 echo "<p>Hi, $yourName, please try again. </p>";
@@ -36,23 +37,20 @@ echo "<p>Hi, $yourName, please try again. </p>";
 //if the View all button is clicked
 if (isset($_POST['view']))
 {
-	$query = "SELECT * FROM visitor";
-	$result = mysqli_query($conn,$query);
-	if(mysqli_num_rows($result)>0)
-	{
-		$display = "<h2>All Visitors </h2>";
-		while($row=mysqli_fetch_assoc($result)){
-			$display .="Name: ".$row["visitorName"]."<br>";
-			$display .="Date Time: " .$row["visitTime"]."<br>";
-			}
-			}
-			echo $display;
-			}
-			//close connection 
+$query = "SELECT * FROM visitor";
+$result = mysqli_query($conn,$query);
+if(mysqli_num_rows($result)>0)
+{
+$display = "<h2>All Visitors </h2>";
+while($row=mysqli_fetch_assoc($result)){
+$display .="Name: ".$row["visitorName"]."<br>";
+$display .="Date Time: " .$row["visitTime"]."<br>";
+}
+}
+echo $display;
+}
+//close connection 
 mysqli_close($conn);
-
 ?>
 </body>
 </html>
-
-
