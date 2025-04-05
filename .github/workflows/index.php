017e72d021f1921a306cb3550766f8ee28cbb34c
jojofaggoo <156486878+jojofaggoo@users.noreply.github.com>
2025-04-05 17:19:18 -0500

    Update index.php

--- .github/workflows/index.php
+++ .github/workflows/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang = "en">
 <head>
 <meta charset = "utf-8">
 <title>My PHP Website</title>
@@ -24,31 +24,23 @@ $db = "visitordb";
 $conn = mysqli_init();
 mysqli_real_connect($conn, $host, $user, $password, $db);
 if (isset($_POST['submit']))
-{
-$yourName=$_POST['name'];
+{$yourName=$_POST['name'];
 //sql statement
 $query = "INSERT INTO visitor (visitorName) VALUES ('$yourName')";
 if(mysqli_query($conn, $query))
-{
-echo "<p>Hi, $yourName, welcome to my cloud.</p>";
+{echo "<p>Hi, $yourName, welcome to my cloud.</p>";
 else
-echo "<p>Hi, $yourName, please try again. </p>";
-}}
+echo "<p>Hi, $yourName, please try again. </p>";}}
 //if the View all button is clicked
-if (isset($_POST['view'])
-{
-$query = "SELECT * FROM visitor";
+if (isset($_POST['view']))
+{$query = "SELECT * FROM visitor";
 $result = mysqli_query($conn,$query);
 if(mysqli_num_rows($result)>0)
-{
-$display = "<h2>All Visitors </h2>";
+{$display = "<h2>All Visitors </h2>";
 while($row=mysqli_fetch_assoc($result)){
 $display .="Name: ".$row["visitorName"]."<br>";
-$display .="Date Time: " .$row["visitTime"]."<br>";
-}
-}
+$display .="Date Time: " .$row["visitTime"]."<br>";}}}
 echo $display;
-}
 //close connection 
 mysqli_close($conn);
 ?>
