31721930edf083585229350a1e8bec87709a1d53
jojofaggoo <156486878+jojofaggoo@users.noreply.github.com>
2025-04-06 07:12:27 -0500

    Update index.php

--- indexa.php
+++ indexa.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>My Cloud Website</title>
+</head>
+<body>
+<h1>Welcome to My Cloud!</h1>
+<p>CISS301 Operating Systems and Cloud Computing</p>
+<p>Instructor: Prof. Alfred Basta</p>
+<p>Dropbox Assignment 7</p>
+<p>Joe Fague</p>
+<br><br><br><br>  
+<form action = "" method ="post">
+Your name:
+<br>
+<input type="text" name = "name" size="30" maxlength = "30">
+<br>
+<input type="submit" name = "submit" value="Submit">
+<input type="submit" name = "view" value = "View All">
+</form>
 <?php
 //replace the user and password with your credentials
 $host = "jfsecondserverfordropbox7.mysql.database.azure.com";
@@ -40,4 +61,6 @@ echo $display;
 //close connection 
 mysqli_close($conn);
 ?>
+</body>
+</html>
 
