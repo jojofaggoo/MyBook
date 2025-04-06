32d826f6d933e85e89011766b7b76fd66868f67d
jojofaggoo <156486878+jojofaggoo@users.noreply.github.com>
2025-04-06 06:58:18 -0500

    Update and rename index.php to indexa.php

--- indexa.php
+++ indexa.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset = "utf-8">
-<title>My PHP Website</title>
-</head>
-<body>
-<h1>Welcome to the Cloud!</h1>
-<form action = "" method ="post">
-Your name:
-<br>
-<input type="text" name = "name" size="30" maxlength = "30">
-<br>
-<input type="submit" name = "submit" value="Submit">
-<input type="submit" name = "view" value = "View All">
-</form>
 <?php
 //replace the user and password with your credentials
 $host = "jfsecondserverfordropbox7.mysql.database.azure.com";
@@ -56,5 +40,4 @@ echo $display;
 //close connection 
 mysqli_close($conn);
 ?>
-</body>
-</html>
+
