e1ac79a1d0b86329f49fd61102cc0ca13bb1b1eb
jojofaggoo <156486878+jojofaggoo@users.noreply.github.com>
2025-04-05 22:38:04 -0500

    Update createdb.php

--- .github/workflows/createdb.php
+++ .github/workflows/createdb.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Create MySQL on Azure</title>
+<meta charset="utf-8">
+<title>Create MySQL on Azure</title>
 </head>
 <body>
 <?php 
@@ -15,25 +15,25 @@ $db = "visitordb";
 // Connect to the database
 $conn = mysqli_init();
 if (!mysqli_real_connect($conn, $host, $user, $password, $db)) {
-    die("Connection failed: " . mysqli_connect_error());
+die("Connection failed: " . mysqli_connect_error());
 }
 // Drop the table if it exists
 $dropQuery = "DROP TABLE IF EXISTS visitor;";
 if (!mysqli_query($conn, $dropQuery)) {
-    echo "<p>Error dropping table: " . mysqli_error($conn) . "</p>";
+echo "<p>Error dropping table: " . mysqli_error($conn) . "</p>";
 }
 
 $query = "CREATE TABLE visitor (
-    visitorid INTEGER AUTO_INCREMENT,
-    visitorName VARCHAR(100) NOT NULL,
-    visitTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(visitorid)
+visitorid INTEGER AUTO_INCREMENT,
+visitorName VARCHAR(100) NOT NULL,
+visitTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY(visitorid)
 )";
 
 if (mysqli_query($conn, $query)) {
-    echo "<p>Table Created.</p>";
+echo "<p>Table Created.</p>";
 } else {
-    echo "<p>Error creating table: " . mysqli_error($conn) . "</p>";
+echo "<p>Error creating table: " . mysqli_error($conn) . "</p>";
 }
 
 // Close connection
