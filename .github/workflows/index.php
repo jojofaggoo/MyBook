ab0ee16a7c91ed9d0a8dedcebd1d7ff3e1bb0ecc
jojofaggoo <156486878+jojofaggoo@users.noreply.github.com>
2025-04-05 22:38:28 -0500

    Update index.php

--- .github/workflows/index.php
+++ .github/workflows/index.php
@@ -26,29 +26,29 @@ mysqli_real_connect($conn, $host, $user, $password, $db);
 $display = ""; // Initialize the display variable
 
 if (isset($_POST['submit'])) {
-    $yourName = $_POST['name'];
-    //sql statement
-    $query = "INSERT INTO visitor (visitorName) VALUES ('$yourName')";
-    if (mysqli_query($conn, $query)) {
-        echo "<p>Hi, $yourName, welcome to my cloud.</p>";
-    } else {
-        echo "<p>Hi, $yourName, please try again. </p>";
-    }
+$yourName = $_POST['name'];
+//sql statement
+$query = "INSERT INTO visitor (visitorName) VALUES ('$yourName')";
+if (mysqli_query($conn, $query)) {
+echo "<p>Hi, $yourName, welcome to my cloud.</p>";
+} else {
+echo "<p>Hi, $yourName, please try again. </p>";
+}
 }
 
 //if the View all button is clicked
 if (isset($_POST['view'])) {
-    $query = "SELECT * FROM visitor";
-    $result = mysqli_query($conn, $query);
-    if (mysqli_num_rows($result) > 0) {
-        $display = "<h2>All Visitors </h2>";
-        while ($row = mysqli_fetch_assoc($result)) {
-            $display .= "Name: " . $row["visitorName"] . "<br>";
-            $display .= "Date Time: " . $row["visitTime"] . "<br>";
-        }
-    } else {
-        $display .= "<p>No visitors found.</p>"; // Add a message if no visitors are found
-    }
+$query = "SELECT * FROM visitor";
+$result = mysqli_query($conn, $query);
+if (mysqli_num_rows($result) > 0) {
+$display = "<h2>All Visitors </h2>";
+while ($row = mysqli_fetch_assoc($result)) {
+$display .= "Name: " . $row["visitorName"] . "<br>";
+$display .= "Date Time: " . $row["visitTime"] . "<br>";
+}
+} else {
+$display .= "<p>No visitors found.</p>"; // Add a message if no visitors are found
+}
 }
 
 echo $display;
